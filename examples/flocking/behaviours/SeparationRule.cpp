8899a7015012e9e160510164a9369bebce718329
Jake Sanderson <jake.sanderson@mymail.champlain.edu>
2024-09-02 11:27:34 -0400

    Started implementing separation rule. Works for now

--- examples/flocking/behaviours/SeparationRule.cpp
+++ examples/flocking/behaviours/SeparationRule.cpp
@@ -7,14 +7,36 @@ Vector2f SeparationRule::computeForce(const std::vector<Boid*>& neighborhood, Bo
   // Try to avoid boids too close
   Vector2f separatingForce = Vector2f::zero();
 
-  //    float desiredDistance = desiredMinimalDistance;
-  //
-  //    // todo: implement a force that if neighbor(s) enter the radius, moves the boid away from it/them
-  //    if (!neighborhood.empty()) {
-  //        Vector2f position = boid->transform.position;
-  //        int countCloseFlockmates = 0;
-  //        // todo: find and apply force only on the closest mates
-  //    }
+    float desiredDistance = desiredMinimalDistance;
+
+    /*// todo: implement a force that if neighbor(s) enter the radius, moves the boid away from it/them
+    if (!neighborhood.empty()) {
+        Vector2f position = boid->transform.position;
+        int countCloseFlockmates = 0;
+        // todo: find and apply force only on the closest mates
+    }*/
+
+  for (const Boid* boidTmp : neighborhood)
+  {
+    //Remove self
+    if(boidTmp->getPosition() == boid->getPosition()) {
+      continue;
+    }
+
+    //Calc Separation vector
+    Vector2f separationVec = boid->getPosition() - boidTmp->getPosition();
+
+    //Calc distance
+    float distance = sqrt((separationVec.x * separationVec.x) + (separationVec.y * separationVec.y));
+
+    //If inside sep radius add force
+    if (distance < desiredDistance && distance > 0.01f) {
+      //normalize sep vector
+      separationVec = separationVec / distance;
+      float forceApply = 1 / distance;
+      separatingForce = separationVec * forceApply;
+    }
+  }
 
   separatingForce = Vector2f::normalized(separatingForce);
 
