4e41219fc6b7f68460af7cf899ce08b4d33889c2
Jake Sanderson <jake.sanderson@mymail.champlain.edu>
2024-09-09 11:25:06 -0400

    started game of life

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,9 +3,30 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
+  int neighborsCount, y, x;
+
+  for (y = 0; y < world.SideSize(); y++) {
+    for (x = 0; x < world.SideSize(); x++) {
+      Point2D pointTmp = Point2D(x, y);
+      neighborsCount = CountNeighbors(world, pointTmp);
+
+      //Check neighbor size
+    }
+  }
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   // todo: implement
-  return 0;
+int count = 0;
+
+  for (int y = -1; y <= 1; y++) {
+    for (int x = -1; x <= 1; x++) {
+      Point2D pointTmp = Point2D(point.x + x, point.y + y);
+      //Check if in bounds
+      //Mirroring
+      count += world.Get(pointTmp);
+    }
+  }
+
+  return count;
 }
