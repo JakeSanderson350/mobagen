c6db0c7e42afb52ff93246c454d966bfac94f5a0
Jake Sanderson <jake.sanderson@mymail.champlain.edu>
2024-09-12 11:25:29 -0400

    fixed neighbor counting function and implemented step function

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,16 +3,34 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
-  int neighborsCount, y, x;
+  int neighborsCount;
 
-  for (y = 0; y < world.SideSize(); y++) {
-    for (x = 0; x < world.SideSize(); x++) {
+  for (int y = 0; y < world.SideSize(); y++) {
+    for (int x = 0; x < world.SideSize(); x++) {
       Point2D pointTmp = Point2D(x, y);
       neighborsCount = CountNeighbors(world, pointTmp);
 
       //Check neighbor size
+      if (world.Get(pointTmp)) {
+        if(neighborsCount < 2) {
+          world.SetNext(pointTmp, false);
+        }
+        else if (neighborsCount <= 3) {
+          world.SetNext(pointTmp, true);
+        }
+        else if (neighborsCount > 3) {
+          world.SetNext(pointTmp, false);
+        }
+      }
+      else {
+        if (neighborsCount == 3) {
+          world.SetNext(pointTmp, true);
+        }
+      }
     }
   }
+
+  world.SwapBuffers();
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
@@ -24,7 +42,11 @@ int count = 0;
       Point2D pointTmp = Point2D(point.x + x, point.y + y);
       //Check if in bounds
       //Mirroring
-      count += world.Get(pointTmp);
+      if(y == 0 && x == 0) {
+      }
+      else {
+        count += world.Get(pointTmp);
+      }
     }
   }
 
